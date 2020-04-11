2635ee45d530862f26d94a75db44386806a4bcb3
Elliot Blanchard <public@elliotblanchard.com>
2020-04-11 17:39:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 katz_deli = []
 
 def line(queue)
-  if (queue.size > 1) && (queue[0] != "")
+  if (queue.size == 1) && (queue[0] != "")
     "The line is currently empty."
   else
     response = "The line is currently: "
@@ -14,4 +14,10 @@ end
 def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{index+1} in line."
+end
+
+def now_serving(katz_deli)
+  puts "Currently serving #{katz_deli.first}."
+  katz_deli.shift
+  katz_deli
 end
