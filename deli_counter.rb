2910c28abe4a0e6e5ebab52f8e54eca2170ced13
Elliot Blanchard <public@elliotblanchard.com>
2020-04-11 17:29:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+katz_deli = []
+
+def line(queue)
+  if (queue.size > 1) && (queue[0] != "")
+    "The line is currently empty."
+  else
+    response = "The line is currently: "
+    queue.each_with_index {|name,index| response << "#{index+1}.  #{name} "}
+    response
+  end
+end
